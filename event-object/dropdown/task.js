3652f48c173582676460932473b149c173b0f5ae
Gorelih Denis <nppfan96@mail.ru>
2024-09-24 14:44:39 +0500

    homework12

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,1 +1,22 @@
-console.log(alert("hello"));
+document.addEventListener('DOMContentLoaded', () => {
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+    dropdowns.forEach(dropdown => {
+        const valueElement = dropdown.querySelector('.dropdown__value');
+        const listElement = dropdown.querySelector('.dropdown__list');
+
+        valueElement.addEventListener('click', () => {
+            listElement.classList.toggle('dropdown__list_active');
+        });
+
+        const items = Array.from(dropdown.querySelectorAll('.dropdown__item'));
+
+        items.forEach(item => {
+            item.addEventListener('click', (event) => {
+                event.preventDefault();
+                valueElement.textContent = item.textContent;
+                listElement.classList.remove('dropdown__list_active');
+            });
+        });
+    });
+});
