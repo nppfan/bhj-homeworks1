5d9261db6adf4a431c82fbfa69c99394f4264955
Gorelih Denis <nppfan96@mail.ru>
2024-10-27 17:25:15 +0500

    homework12

---
+++ event-object/tabs/task.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const tabContainers = document.querySelectorAll('.tab__navigation');
+
+    tabContainers.forEach(container => {
+        const tabs = container.querySelectorAll('.tab');
+        const contents = document.querySelectorAll('.tab__content');
+
+        tabs.forEach((tab, index) => {
+            tab.addEventListener('click', () => {
+                tabs.forEach(t => t.classList.remove('tab_active'));
+                contents.forEach(c => c.classList.remove('tabcontent_active'));
+                tab.classList.add('tab_active');
+                contents[index].classList.add('tabcontent_active');
+            });
+        });
+    });
+});
