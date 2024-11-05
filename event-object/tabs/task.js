e34ce56331658d2be07b1b6a6d49620838ca0599
Gorelih Denis <nppfan96@mail.ru>
2024-11-05 13:08:04 +0500

    homework12

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -3,14 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     tabContainers.forEach(container => {
         const tabs = container.querySelectorAll('.tab');
-        const contents = container.nextElementSibling.querySelectorAll('.tab__content'); 
+        const contents = container.nextElementSibling.querySelectorAll('.tab__content');
 
         tabs.forEach((tab, index) => {
             tab.addEventListener('click', () => {
                 tabs.forEach(t => t.classList.remove('tab_active'));
-                contents.forEach(c => c.classList.remove('tabcontent_active'));
+                contents.forEach(c => c.classList.remove('tab__content_active'));
                 tab.classList.add('tab_active');
-                contents[index].classList.add('tabcontent_active');
+                contents[index].classList.add('tab__content_active');
             });
         });
     });
